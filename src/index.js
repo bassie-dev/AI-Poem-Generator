fdf33f76c46c530b1771a3d50d09f780674266d4
Basetsana Mokwena <mokwenabasetsana022@gmail.com>
2026-08-16 12:20:12 +0200

    added Api Integration and prompt engineering

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 function SearchPoem(event){
     event.preventDefault();
+    let instructionsInput = document.querySelector("#user-instructions")
+ let apiKey = "c5f9o2bda4fat8f0a304ce0a3b4bb317";
+ let prompt = `Generate a poem about ${instructionsInput.value}`;
+ let context =
+   "You are a creative poet. Provide a 8 lines poem and separate the lines using <br /> tags.choose only one poem";
+let apiUrl =
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;   
+axios.get(apiUrl).then(showPoem);
+}
+function showPoem(response){
 new Typewriter("#poem", {
-  strings: "The fountains mingle with the river",
+  strings: response.data.answer,
   autoStart: true,
-  delay: 1,
+  delay: 20,
   cursor:""
 });
 }
