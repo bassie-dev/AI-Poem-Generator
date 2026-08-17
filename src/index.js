0a0196e048a0a2766f145c7a15bd538272596d1f
Basetsana Mokwena <mokwenabasetsana022@gmail.com>
2026-08-17 17:39:40 +0200

    updated User Experience

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-function SearchPoem(event){
-    event.preventDefault();
-    let instructionsInput = document.querySelector("#user-instructions")
- let apiKey = "c5f9o2bda4fat8f0a304ce0a3b4bb317";
- let prompt = `Generate a poem about ${instructionsInput.value}`;
- let context =
-   "You are a creative poet. Provide a 8 lines poem and separate the lines using <br /> tags.choose only one poem";
-let apiUrl =
-  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;   
-axios.get(apiUrl).then(showPoem);
+function SearchPoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let poemElement = document.querySelector("#poem");
+
+  poemElement.innerHTML = `<div class="blink">⏳ Writing a poem about <strong>${instructionsInput.value}</strong>...</div>`;
+
+  let apiKey = "c5f9o2bda4fat8f0a304ce0a3b4bb317";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a creative poet. Provide a 8 lines poem and separate the lines using <br /> tags.choose only one poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(showPoem);
 }
-function showPoem(response){
-new Typewriter("#poem", {
-  strings: response.data.answer,
-  autoStart: true,
-  delay: 20,
-  cursor:""
-});
+function showPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
 }
 
-let formElement=document.querySelector("#poem-form")
-formElement.addEventListener("submit",SearchPoem)
+let formElement = document.querySelector("#poem-form");
+formElement.addEventListener("submit", SearchPoem);
